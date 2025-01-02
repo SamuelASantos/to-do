14b094b2ed7c24d304b685813b0efeb99d21cc9e
Samuel Santos <samuel_a_santos@hotmail.com>
2025-01-02 00:03:41 -0300

    Atualizado o script.js e o style.css
    - Atualizado a função de renderizar a lista
    - Adicionado as funções para adicionar as tarefas tanto clicando no botão como apertando Enter
    - Adicionado classe no css de estilização para tarefas concluídas
    - Concluído o projeto 1.0 de lista to-do

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,47 @@
-let list = document.querySelector('.task');
-let input_text = document.querySelector('#tasks');
+var list = document.querySelector('.task');
+let inputText = document.querySelector('#tasks');
+inputText.addEventListener('keyup', (event) => {
+    if (event.key.toLowerCase() === 'enter') {
+        addTask();
+    }
+})
 
-let tasks = [
-    { done: false, title: 'Fazer academia' },
-    { done: true, title: 'Estudar programação' },
-    { done: false, title: 'Fazer a feira' }
-];
+let tasks = [];
+
+function addTask() {
+    if (inputText.value !== '') {
+        tasks.push({
+            done: false,
+            title: inputText.value
+        });
+    }
+    inputText.value = '';
+    renderList();
+}
 
 function renderList() {
     list.innerHTML = '';
 
     for (let i in tasks) {
         let taskLi = document.createElement('li');
-        taskLi.innerHTML = tasks[i].title;
+        let taskInput = document.createElement('input');
+        let taskSpan = document.createElement('span');
+
+        taskInput.setAttribute('type', ('checkbox'));
+        if (tasks[i].done) {
+            taskInput.setAttribute('checked', 'true');
+            taskLi.classList.add('done');
+        }
+
+        taskInput.addEventListener('click', () => {
+            tasks[i].done = !tasks[i].done;
+            renderList();
+        });
+
+        taskLi.appendChild(taskInput);
+
+        taskSpan.innerHTML = tasks[i].title;
+        taskLi.appendChild(taskSpan);
 
         list.appendChild(taskLi);
     }
